1d0dae50bd0cbb15d89cce4104fd064337fd0d6d
sylvainJnt29 <sylvain.jnt@gmail.com>
2022-04-05 22:02:51 +0200

    prochaine étape comparo des listes :)

--- app.js
+++ app.js
@@ -5,31 +5,22 @@ Problème : certains ARIs ne répondent pas à Salt ( donc absent de la liste Sa
 IL faut trouver ces ARIS présent dans la liste FLEET et PAS présent dans la liste Salt
 Il faut trouver les ARIs dans la NMAP et qui sont pas dans SALt 
 
-
-! Manque le dernier [i] de tabFleet 8e8bdec4 !
-! liste salt fonctionnelle !
+Reste à faire:
 ! https://openclassrooms.com/forum/sujet/javascript-comparaison-de-deux-tableaux !
 => méthode filter 
 
+Notes: Ameliorer #reponse3 et #reponse4
+        => Mise en page sur du innerHTML
 
+    // regrouper deux tableaux dans un seul => utilité ?
+    // Array.prototype.push.apply(tabFleet, tabSalt);
+    // alert(tabFleet);
+    // alert(tabFleet.sort());
 
-        
-A CREUSER:
-L'utilisation du constructeur new serait plus indiquée pour la recherche de caractères 
 
 */
 
-
-
-
-    // Les regExp
-
-    var regexFleet = /[\w]{8}[\n]/g;
-    // var regexFleet = new RegExp ("/[\w]{8}[\n]/");
-    var regexSaltNmap = /[a-z0-9]{8}/;
-    // [^A-Z: -0{8}]
-
-
+    // Déclaration des tableaux
     var tabFleet=[];
     var tabSalt=[];
     var tabNmap=[];
@@ -40,23 +31,21 @@ L'utilisation du constructeur new serait plus indiquée pour la recherche de car
     var entreeNmap = document.getElementById('texte3').value;
 
     // Utilisation d'une regExp
-    var testRegexFleet = regexFleet.exec(entreeFleet);
-    var testRegexSalt = regexSaltNmap.exec(entreeSalt);
-    var testRegexNmap = regexSaltNmap.exec(entreeNmap);
+    // var testRegexFleet = regexFleet.exec(entreeFleet);
+    // var testRegexSalt = regexSaltNmap.exec(entreeSalt);
+    // var testRegexNmap = regexSaltNmap.exec(entreeNmap);
 
-    // Affichage du retour de la saisie ( test )
-    document.getElementById('reponse').innerHTML = entreeFleet;
-    document.getElementById('reponse2').innerHTML = entreeSalt;
-    document.getElementById('reponse3').innerHTML = entreeNmap;
+    // // Affichage du retour de la saisie ( test )
+    // document.getElementById('reponse').innerHTML = entreeFleet;
+    // document.getElementById('reponse2').innerHTML = entreeSalt;
+    // document.getElementById('reponse3').innerHTML = entreeNmap;
 
 
 
     function check(){
 
         // On parcourt et on enregistre dans "tabFleet" les N° des ARIs trouvés dans la liste fleet
-        // var fleetRegex = /[\w]{8}[\n]/g;
-        // var fleetRegex = /[\w]{8}/g;
-        var fleetRegex = /[^test_tube,warning,:,Ch.#][\w]{8}/g;
+        var fleetRegex = /[^test_tube,warning:Ch.#][\w]{8}/g;
         var fleetStr = entreeFleet;
         var monTableauFleet=[""];
         var tabFleet=[""];
@@ -67,10 +56,9 @@ L'utilisation du constructeur new serait plus indiquée pour la recherche de car
         fleetMsg +=
         console.log(fleetMsg);
         }
-        alert("Nombre de ARI(s) extrait(s) de la liste fleet : " + (tabFleet.length-1));
+
 
         // On parcourt et on enregistre dans "tabSalt" les N° des ARIs trouvés dans la liste salt
-        // var saltRegex = /[0-9A-Za-z]{8}:/g;
         var saltRegex = /((?<=00000000)[\w]{8})/g;
         var saltStr = entreeSalt;
         var monTableauSalt=[""];
@@ -82,14 +70,10 @@ L'utilisation du constructeur new serait plus indiquée pour la recherche de car
         saltMsg +=
         console.log(saltMsg);
         }
-        alert("Nombre de ARI(s) extrait(s) de la liste salt : " + (tabSalt.length-1));
+
 
         // On parcourt et on enregistre dans "tabNmap" les N° des ARIs trouvés dans la liste Nmap
-        // var nmapRegex = /[0-9A-Za-z]{8} /g;
-        // var nmapRegex = /[0-9A-Za-z]{16}/g;
         var nmapRegex = /((?<=00000000)[\w]{8})/g;
-
-    
         var nmapStr = entreeNmap;
         var monTableauNmap=[""];
         var tabNmap=[""];
@@ -100,30 +84,41 @@ L'utilisation du constructeur new serait plus indiquée pour la recherche de car
         nmapMsg +=
         console.log(nmapMsg);
         }
-        alert("Nombre de ARI(s) extrait(s) de la liste nmap : " + (tabNmap.length-1));
-        alert(tabFleet);
-        alert(tabSalt);
-        alert(tabNmap);
+
+
+        // Affichage des résultats
+        document.getElementById('reponse').innerHTML = "<h1>Les résultats :</h1>";
+
+        document.getElementById('reponse2').innerHTML += "Nombre de ARI(s) extrait(s) de la liste fleet : <b>"
+                                                      +(tabFleet.length-1)+"</b>";
+        document.getElementById('reponse2').innerHTML += "<h2>Extrait de fleet :</h2>" + tabFleet;
+
+        document.getElementById('reponse3').innerHTML += "Nombre de ARI(s) extrait(s) de la liste salt : <b>"
+                                                      +(tabSalt.length-1)+"</b>";
+        document.getElementById('reponse3').innerHTML += "<h2>Extrait de salt :</h2>" + tabSalt;
+
+        document.getElementById('reponse4').innerHTML += "Nombre de ARI(s) extrait(s) de la liste nmap : <b>"
+                                                      +(tabNmap.length-1)+"</b>";
+        document.getElementById('reponse4').innerHTML += "<h2>Extrait de nmap :</h2>" + tabNmap;
+
 
 
         // Comparaison des correspondances des tableaux fleet et salt:
-        // => IL faut trouver ces ARIS présent dans la liste FLEET et PAS présent dans la liste Salt
+        // => IL faut trouver ces ARIS présents dans la liste FLEET et PAS présent dans la liste Salt
 
-        // var a1 = [tabFleet];
-        // var a2 = [tabSalt];
-        // var a3 = [tabNmap];
+        // Zone de test:
+        document.getElementById('reponse3').innerHTML += "<h2>ARIS présents dans la liste fleet et non présents dans la liste salt:</h2>";
+        document.getElementById('reponse3').innerHTML += "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
 
-        // console.log("Compare a1 et a2", JSON.stringify(a1) === JSON.stringify(a2));
-        // console.log("Compare a2 et a3", JSON.stringify(a2) === JSON.stringify(a3));
 
-        // regrouper deux tableaux dans un seul => utilité ?
-        // Array.prototype.push.apply(tabFleet, tabSalt);
-        // alert(tabFleet);
-        // alert(tabFleet.sort());
+
 
         // Comparaison des correspondances des tableaux salt et nmap:
         // => Il faut trouver les ARIs dans la NMAP et qui sont pas dans SALt 
 
+        // Zone de test:
+        document.getElementById('reponse4').innerHTML += "<h2>ARIS présents dans la liste nmap et non présents dans la liste salt:</h2>";
+        document.getElementById('reponse4').innerHTML += "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
 
     }
 
